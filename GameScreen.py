4f7fef4dffa39c190b8d3a1e60a466efacfd6144
srartese <saa2457@rit.edu>
2018-01-28 15:54:54 -0500

    UI and image loading

--- GameScreen.py
+++ GameScreen.py
@@ -8,50 +8,30 @@ from pygame.locals import *
 # set up pygame
 pygame.init()
 
-# set up the window
-windowSurface = pygame.display.set_mode((500, 400), 0, 32)
-pygame.display.set_caption('Hello world!')
+#Set up the window
+windowSurface = pygame.display.set_mode((900, 700), 0 , 32)
+pygame.display.set_caption('Pick Up the Phone!')
 
-# set up the colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+img = pygame.image.load('images/bphone.png')
+
+# SET COLORS
+WHITE = (255,255,255)
+PEACH = (246,176,146)
 
 # set up fonts
 basicFont = pygame.font.SysFont(None, 48)
 
-# set up the text
-text = basicFont.render('Hello world!', True, WHITE, BLUE)
+#Set up the text
+text = basicFont.render('Pick Up the Phone!', True, WHITE)
 textRect = text.get_rect()
 textRect.centerx = windowSurface.get_rect().centerx
-textRect.centery = windowSurface.get_rect().centery
+textRect.centery = 70
 
 # draw the white background onto the surface
-windowSurface.fill(WHITE)
+windowSurface.fill(PEACH)
 
-# draw a green polygon onto the surface
-pygame.draw.polygon(windowSurface, GREEN, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
-
-# draw some blue lines onto the surface
-pygame.draw.line(windowSurface, BLUE, (60, 60), (120, 60), 4)
-pygame.draw.line(windowSurface, BLUE, (120, 60), (60, 120))
-pygame.draw.line(windowSurface, BLUE, (60, 120), (120, 120), 4)
-
-# draw a blue circle onto the surface
-pygame.draw.circle(windowSurface, BLUE, (300, 50), 20, 0)
-
-# draw a red ellipse onto the surface
-pygame.draw.ellipse(windowSurface, RED, (300, 250, 40, 80), 1)
-
-# draw the text's background rectangle onto the surface
-pygame.draw.rect(windowSurface, RED, (textRect.left - 20, textRect.top - 20, textRect.width + 40, textRect.height + 40))
-
-# get a pixel array of the surface
-pixArray = pygame.PixelArray(windowSurface)
-pixArray[480][380] = BLACK
-del pixArray
+#Load in bananaphone image
+windowSurface.blit(img, (windowSurface.get_rect().centerx-180,170))
 
 # draw the text onto the surface
 windowSurface.blit(text, textRect)
@@ -65,6 +45,8 @@ ts = TouchSensor()
 # run the game loop
 while True:
     ts.update()
+
+
     if ts.status(3) == ts.TOUCHED:
         print("WOOOO")
     for event in pygame.event.get():
